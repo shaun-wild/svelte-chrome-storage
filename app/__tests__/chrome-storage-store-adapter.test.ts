3ca8aba89ee3056b84df0c245d2773eff608a542
Shaun Wild <shaunwild97@gmail.com>
2022-07-17 05:23:21 +0100

    Initial commit

---
+++ app/__tests__/chrome-storage-store-adapter.test.ts
@@ -0,0 +1,79 @@
+import {chrome} from 'jest-chrome'
+import {chromeStorageLocal, chromeStorageManaged, chromeStorageSession, chromeStorageSync} from "../chrome-storage-store-adapter";
+
+describe('chrome storage store adapter', () => {
+    it('should register a store sync', () => {
+        const store = chromeStorageSync<string>("test")
+        const callback = jest.fn()
+
+        store.subscribe(callback)
+        chrome.storage.onChanged.callListeners({test: {newValue: "newTest", oldValue: "test"}}, "sync")
+
+        expect(callback).toHaveBeenCalledWith("newTest")
+    })
+
+    it('should register a store local', () => {
+        const store = chromeStorageLocal<string>("test")
+        const callback = jest.fn()
+
+        store.subscribe(callback)
+        chrome.storage.onChanged.callListeners({test: {newValue: "newTest", oldValue: "test"}}, "local")
+
+        expect(callback).toHaveBeenCalledWith("newTest")
+    })
+
+    it('should register a store session', () => {
+        const store = chromeStorageSession<string>("test")
+        const callback = jest.fn()
+
+        store.subscribe(callback)
+        chrome.storage.onChanged.callListeners({test: {newValue: "newTest", oldValue: "test"}}, "session")
+
+        expect(callback).toHaveBeenCalledWith("newTest")
+    })
+
+    it('should register a store managed', () => {
+        const store = chromeStorageManaged<string>("test")
+        const callback = jest.fn()
+
+        store.subscribe(callback)
+        chrome.storage.onChanged.callListeners({test: {newValue: "newTest", oldValue: ""}}, "managed")
+
+        expect(callback).toHaveBeenCalledWith("newTest")
+    })
+
+    it('should set chrome storage local', () => {
+        const store = chromeStorageLocal<string>("test")
+        store.set("newTest")
+        expect(chrome.storage.local.set).toHaveBeenCalledWith({test: "newTest"})
+    })
+
+    it('should set chrome storage sync', () => {
+        const store = chromeStorageSync<string>("test")
+        store.set("newTest")
+        expect(chrome.storage.sync.set).toHaveBeenCalledWith({test: "newTest"})
+    })
+
+    it('should fail to set managed store', () => {
+        const store = chromeStorageManaged<string>("test")
+        expect(() => store.set("newValue")).toThrowError()
+    })
+
+    it('should update a value', () => {
+        const store = chromeStorageLocal<string>("test")
+        chrome.storage.local.get.mockImplementation((key, cb) => cb({test: "123"}))
+        store.update(value => value + "test")
+        expect(chrome.storage.local.set).toHaveBeenCalledWith({test: "123test"})
+    })
+
+    it('should unsubscribe', () => {
+        const store = chromeStorageLocal<string>("test")
+        const callback = jest.fn()
+
+        const unsubscribe = store.subscribe(callback)
+        unsubscribe()
+        chrome.storage.onChanged.callListeners({test: {newValue: "newTest", oldValue: "test"}}, "local")
+
+        expect(callback).toHaveBeenCalledWith("newTest")
+    })
+})
---
+++ app/chrome-storage-store-adapter.ts
@@ -0,0 +1,63 @@
+import type {Subscriber, Unsubscriber, Updater, Writable} from "svelte/store";
+import AreaName = chrome.storage.AreaName;
+
+type AdapterDictionary = { [key: string]: Array<Subscriber<any>> }
+
+const adapters: { [key in AreaName]: AdapterDictionary } = {
+    sync: {},
+    local: {},
+    managed: {},
+    session: {}
+}
+
+chrome.storage.onChanged.addListener((changes, area) => {
+    const areaAdapters = adapters[area]
+    Object.entries(changes).forEach(([key, value]) => {
+        areaAdapters[key].forEach(run => run(value.newValue))
+    })
+})
+
+export function chromeStorageLocal<T>(key: string): ChromeStorageStoreAdapter<T> {
+    return new ChromeStorageStoreAdapter('local', key)
+}
+
+export function chromeStorageSync<T>(key: string): ChromeStorageStoreAdapter<T> {
+    return new ChromeStorageStoreAdapter('sync', key)
+}
+
+export function chromeStorageManaged<T>(key: string): ChromeStorageStoreAdapter<T> {
+    return new ChromeStorageStoreAdapter('managed', key)
+}
+
+export function chromeStorageSession<T>(key: string): ChromeStorageStoreAdapter<T> {
+    return new ChromeStorageStoreAdapter('session', key)
+}
+
+class ChromeStorageStoreAdapter<T> implements Writable<T> {
+    constructor(
+        private area: AreaName,
+        private key: string
+    ) {
+    }
+
+    set(value: T): void {
+        if (this.area === 'managed') {
+            throw Error("Cannot set managed area")
+        }
+
+        chrome.storage[this.area].set({[this.key]: value})
+    }
+
+    subscribe(run: Subscriber<T>): Unsubscriber {
+        const subscriberArray = adapters[this.area][this.key] ?? (adapters[this.area][this.key] = [])
+        const index = subscriberArray.push(run)
+        return () => subscriberArray.splice(index, 1)
+    }
+
+    update(updater: Updater<T>): void {
+        chrome.storage[this.area].get(this.key, item => {
+            const newItem = updater(item[this.key])
+            this.set(newItem)
+        })
+    }
+}
